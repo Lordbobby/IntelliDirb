c6cc1c6bc87c2977562c9b3e9be0379e64c09ff3
Zach McGee <zach@upperlevelcrm.com>
2024-09-28 11:51:28 -0400

    update target handling

--- dirb/target.py
+++ dirb/target.py
@@ -1,8 +1,18 @@
 
+# Ensure slash at end. Will allow just about anything as target string.
+def _parse_input(target_str):
+    if not target_str.endswith('/'):
+        target_str = f'{target_str}/'
+
+    return target_str
+
 class Target:
     def __init__(self, target_str):
-        # TODO parse string
-        self.url = target_str
+        self.url = _parse_input(target_str)
 
-    def get_url(self):
+    def get_base_url(self):
         return self.url
+
+    def build_url(self, path):
+        return f'{self.url}{path}'
+
