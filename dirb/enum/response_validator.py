80964bb577ca0bfc1372497d7e0ca54edb426196
Zach McGee <zach@upperlevelcrm.com>
2024-09-28 12:19:29 -0400

    build out basic response validator

---
+++ dirb/enum/response_validator.py
@@ -0,0 +1,32 @@
+from requests import Response
+
+class ResponseValidator:
+    def __init__(self):
+        self.size_counter = {}
+
+        # anything that indicates a webpage is there
+        self.valid_response_codes = [200, 201, 202, 301, 302, 400, 401, 403, 500]
+        # anything that indicates a webpage is not there
+        self.invalid_response_codes = [404]
+
+    def validate_response(self, response: Response):
+        resp_size = len(response.content)
+
+        if resp_size not in self.size_counter:
+            self.size_counter[resp_size] = 1
+
+            return True
+
+        self.size_counter[resp_size] = self.size_counter[resp_size] + 1
+
+        # Check if this is a constant response size, may indicate a not found page returning a valid response code
+        if self.size_counter[resp_size] > 5:
+            return False
+
+        if response.status_code in self.valid_response_codes:
+            return True
+
+        if response.status_code in self.invalid_response_codes:
+            return False
+
+        return False
