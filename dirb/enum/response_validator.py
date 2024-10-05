62f3828d3d73cee1bd40515a3a492d725394488d
Zach McGee <zach@upperlevelcrm.com>
2024-10-05 14:29:44 -0400

    Bug fixes to invalidate 404 responses

--- dirb/enum/response_validator.py
+++ dirb/enum/response_validator.py
@@ -7,7 +7,12 @@ class ResponseValidator:
         # anything that indicates a webpage is there
         self.valid_response_codes = [200, 201, 202, 301, 302, 400, 401, 403, 405, 500]
 
+        self.invalid_response_codes = [404]
+
     def validate_response(self, response: Response):
+        if response.status_code in self.valid_response_codes:
+            return False
+
         resp_size = len(response.content)
 
         if resp_size not in self.size_counter:
