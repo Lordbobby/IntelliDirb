7845b6c5938321f8b436418e7d46bd0b6310f5ce
Zach McGee <zach@upperlevelcrm.com>
2024-10-05 14:29:13 -0400

    Bug fixes related to workers not exiting

--- dirb/enum/http_client_worker.py
+++ dirb/enum/http_client_worker.py
@@ -7,13 +7,15 @@ def send_queued_requests(request_queue: Queue, response_queue: Queue, status):
     logger.debug('Spinning up HTTP client worker...')
 
     while status.running:
+        if request_queue.empty():
+            continue
+
         request_url = request_queue.get()
         logger.debug(f'Sending request to: {request_url}')
 
         response = requests.get(request_url)
 
         response_queue.put(response)
-
         request_queue.task_done()
 
     logger.debug('HTTP client worker finished.')
--- dirb/output/output_worker.py
+++ dirb/output/output_worker.py
@@ -5,6 +5,9 @@ def handle_output(output_handler, output_queue, status):
     logger.debug('Spinning up output worker...')
 
     while status.running:
+        if output_queue.empty():
+            continue
+
         message = output_queue.get()
 
         output_handler.send_message(message)
