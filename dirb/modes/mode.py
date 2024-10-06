48fbbb59d7a02e64606dad696991294163a834c8
Zach McGee <zach@upperlevelcrm.com>
2024-10-06 15:14:31 -0400

    Improve recursion handling and stat message

--- dirb/modes/mode.py
+++ dirb/modes/mode.py
@@ -1,4 +1,5 @@
 from queue import Queue
+from time import time_ns
 
 from dirb.enum.response_validator import ResponseValidator
 from dirb.output import logger
@@ -23,6 +24,7 @@ def create_extension_list(extensions):
 class ModeStats:
     requests = 0
     valid_responses = 0
+    start = 0
 
 class Mode:
 
@@ -44,6 +46,7 @@ class Mode:
     def enumerate(self, request_queue, response_queue, output_queue):
         logger.info('Beginning enumeration...')
         output_queue.put(StartMessage())
+        self.stats.start = time_ns()
 
         while self.is_wordlist_not_exhausted() or not request_queue.empty() or not response_queue.empty():
             self.enumerate_wordlist(request_queue, response_queue, output_queue)
@@ -52,7 +55,14 @@ class Mode:
             request_queue.join()
 
         output_queue.put(FinishMessage(self.stats))
-        logger.info(f'Finished enumerating. Sent {Color.BLUE}{self.stats.requests}{Color.RESET} requests. Identified {Color.GREEN}{self.stats.valid_responses}{Color.RESET} valid responses.')
+
+        # Log summary
+        run_time = (time_ns() - self.stats.start) / 1e9
+        run_message = f'Finished enumerating in {Color.BLUE}{run_time:.2f}{Color.RESET} seconds.'
+        requests_message = f'Sent {Color.BLUE}{self.stats.requests}{Color.RESET} requests.'
+        responses_message = f'Identified {Color.GREEN}{self.stats.valid_responses}{Color.RESET} valid responses.'
+
+        logger.info(f'{run_message} {requests_message} {responses_message}')
 
     def enumerate_wordlist(self, request_queue, response_queue, output_queue):
         while self.is_wordlist_not_exhausted() or not request_queue.empty() or not response_queue.empty():
@@ -74,9 +84,11 @@ class Mode:
 
         self.wordlist.reset_index()
         self.current_directory = self.directory_queue.get()
+        logger.debug(f'Reset wordlist with new directory: {self.current_directory}')
 
     def update_request_queue(self, request_queue):
         if self.wordlist.index >= self.wordlist.lines:
+            self.reset_wordlist()
             return
 
         words = self.wordlist.get_words(WORDS_TO_PULL)
@@ -87,9 +99,6 @@ class Mode:
             for word in words:
                 self.stats.requests += 1
                 request_queue.put(f'{self.target}{self.current_directory}{word}{extension}')
-
-        if len(words) < WORDS_TO_PULL:
-            self.reset_wordlist()
 
     def handle_responses(self, response_queue: Queue, output_queue):
         # counter so it doesn't run forever
