20a904e406743c83760e16d7ab84b2eef08ccba8
Zach McGee <zach@upperlevelcrm.com>
2024-10-06 15:33:54 -0400

    add skeleton for different modes

---
+++ dirb/modes/combined.py
@@ -0,0 +1,7 @@
+from dirb.enum.request_queue import RequestQueue
+from dirb.modes.dictionary import Dictionary
+
+class Combined(Dictionary):
+
+    def process_valid_response(self, response, request_queue: RequestQueue, output_queue):
+        super().process_valid_response(response, request_queue, output_queue)
