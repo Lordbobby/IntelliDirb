98331b0e508ec2175f636a4a97e7824dbe280afc
Zach McGee <zach@upperlevelcrm.com>
2024-10-11 09:23:10 -0400

    fix href/src parsers to not include arguments on URLs

--- dirb/modes/parser/href.py
+++ dirb/modes/parser/href.py
@@ -2,4 +2,4 @@ from dirb.modes.parser.regex_based import RegexBasedParser
 
 class HrefParser(RegexBasedParser):
     def __init__(self):
-        super().__init__('href=[\'"]([^\s:#\'"]+)[\'"]')
+        super().__init__('href=[\'"]([^\s:?#\'"]+)[\'"]')
