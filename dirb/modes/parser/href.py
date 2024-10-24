54fb13967d91679ddac5a722630c6571b18c25f7
Zach McGee <zach@upperlevelcrm.com>
2024-10-23 22:11:14 -0400

    improve href attribute regex parsing

--- dirb/modes/parser/href.py
+++ dirb/modes/parser/href.py
@@ -2,4 +2,11 @@ from dirb.modes.parser.regex_based import RegexBasedParser
 
 class HrefParser(RegexBasedParser):
     def __init__(self):
-        super().__init__('href=[\'"]([^\s:?#\'&*"]+)[\'"]')
+        super().__init__('href=[\'"]([^\s:?#\'&*"]+)[^:]\S*[\'"]')
+
+    def find_paths_in_response(self, content, regex):
+        matches = super().find_paths_in_response(content, regex)
+
+        matches = [match for match in matches if not match.startswith('http')]
+
+        return matches
