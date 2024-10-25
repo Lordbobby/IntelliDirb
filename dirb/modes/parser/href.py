27b066c105b228b9ee62b2423d30e65cc153758e
Zach McGee <zach@upperlevelcrm.com>
2024-10-24 20:51:18 -0400

    fix href search not pulling last character

--- dirb/modes/parser/href.py
+++ dirb/modes/parser/href.py
@@ -2,7 +2,7 @@ from dirb.modes.parser.regex_based import RegexBasedParser
 
 class HrefParser(RegexBasedParser):
     def __init__(self):
-        super().__init__('href=[\'"]([^\s:?#\'&*"]+)[^:]\S*[\'"]')
+        super().__init__('href=[\'"]([^\s:?#\'&*"]+)\S*[\'"]')
 
     def find_paths_in_response(self, content, regex):
         matches = super().find_paths_in_response(content, regex)
