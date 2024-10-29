2a72aa2a95ad1d51ebf3722b62e3fdca21d5e004
Zach McGee <zach@upperlevelcrm.com>
2024-10-28 21:53:42 -0400

    add special character to src parsing

--- dirb/modes/parser/src.py
+++ dirb/modes/parser/src.py
@@ -2,4 +2,4 @@ from dirb.modes.parser.regex_based import RegexBasedParser
 
 class SrcParser(RegexBasedParser):
     def __init__(self):
-        super().__init__('src=[\'"]([^\s:?#\'"]+)[\'"]')
+        super().__init__('src=[\'"]([^\s:?#\'"\(!]+)[\'"]')
